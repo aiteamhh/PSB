9a01e6626bef22c8dcaa48dacad8120c0bf95258
Martin We <madingow@gmail.com>
2018-02-22 15:45:55 +0100

    initial commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DottyBot.WebApiApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using Controllers;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace DottyBot
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            //var builder = new ContainerBuilder();
+            //builder.RegisterType<ActivityLogger>().AsImplementedInterfaces().InstancePerDependency();
+            //builder.Update(Conversation.Container);
+        }
+
+    }
+}
